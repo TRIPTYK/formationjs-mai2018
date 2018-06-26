13a9606d2b1c39340b92bba0a026213a7ba9b3e5
Anissa Belkheir <anissa@triptyk.eu>
2018-06-26 14:58:28 +0200

    exo menu

--- jquery/11-exo-menu/js/main.js
+++ jquery/11-exo-menu/js/main.js
@@ -1,5 +1,33 @@
 (function($){
+    $('.submenu').hide();
+
     $(document).ready(function(){
 
+        $('.toggleMenu span').each(function(){
+            var originalText = $(this).text();
+            $(this).replaceWith('<a href="#">'+originalText+'<span class="more" style="float:right">+</span></a>');
+        });
+
+        $('.menu a').on('click', function(){
+            // event.preventDefault();
+            if ($(this).next('.submenu:visible').length == 0) { //menu fermé
+
+                $('.submenu').slideUp();
+                $('.more').text('+');
+                $('li').removeClass('active');
+
+                $(this).next('.submenu').slideDown();
+                $(this).find('.more').text('-');
+                $(this).parent('li').addClass('active');
+
+            } else {
+
+                $(this).next('.submenu').slideUp();
+                $(this).find('.more').text('+');
+                $(this).parent('li').removeClass('active');
+            }
+        });
+
     });
+
 })(jQuery);
